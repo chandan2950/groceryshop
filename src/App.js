63d14ab885f9bdcc4d02805c68d6feff1b41f054
chandan pal <chandanpal18222gmail.com>
2024-05-16 15:13:16 +0530

    homecat

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route, Routes, BrowserRouter} from 'react-router-dom'
 import Home from './PAGES/Homepage/Home';
 import './App.css'
 
+
 export const App = () => {
   return (
     <BrowserRouter>
--- src/Components/Categories/HomeCategories.js
+++ src/Components/Categories/HomeCategories.js
@@ -1,12 +1,50 @@
 import React from 'react'
 import './HomeCategories.css'
+import img1 from '../../Assets/img1.jpg'
+import img2 from '../../Assets/img2.jpg'
+import img3 from '../../Assets/img3.jpg'
+import img4 from '../../Assets/img4.jpg'
+
+
 const HomeCategories = () => {
   return (
-    <div className='HCategorie'>
-        <div className="container"></div>
-        <div className="container"></div>
-        <div className="container"></div>
-        <div className="container"></div>
+    <div className='homecategories'>
+      <div className='container'>
+        <img src={img1} alt='veggie' />
+        <div className='content'>
+          <h1>
+            Vegetables at your doorstep
+          </h1>
+          <p> Shop vegetables now</p>
+        </div>
+      </div>
+      <div className='container'>
+        <img src={img2} alt='veggie' />
+        <div className='content'>
+          <h1>
+            Vegetables at your doorstep
+          </h1>
+          <p> Shop vegetables now</p>
+        </div>
+      </div>
+      <div className='container'>
+        <img src={img3} alt='fruit' />
+        <div className='content'>
+          <h1>
+            Fruits at your doorstep
+          </h1>
+          <p> Shop vegetables now</p>
+        </div>
+      </div>
+      <div className='container'>
+        <img src={img4} alt='fruit' />
+         <div className='content'>
+          <h1>
+            Fruits at your doorstep
+          </h1>
+          <p> Shop vegetables now</p>
+        </div>
+      </div>
     </div>
   )
 }
