2e4123fb3208c20006de790c243547a083e1fb66
chandan pal <chandanpal18222gmail.com>
2024-05-17 22:50:49 +0530

    login,signup,forgotpass

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react'
 import {Route, Routes, BrowserRouter} from 'react-router-dom'
 import Home from './PAGES/Homepage/Home';
 import './App.css'
-
+import Login from './PAGES/Auth/Login';
+import Signup from './PAGES/Auth/Signup';
+import ForgotPassword from './PAGES/Auth/ForgotPassword';
 
 export const App = () => {
   return (
@@ -11,6 +13,9 @@ export const App = () => {
       <Route path='/' element={<Home />}/>
       <Route path='/home' element={<Home />}/>
       <Route path='*' element={<div> oops 404 Page Not Found!!!!!</div>}/>
+      <Route path="/login" element={<Login/>} />
+      <Route path="/signup" element={<Signup/>} />
+      <Route path="/forgotpassword" element={<ForgotPassword/>} />
     </Routes>
     </BrowserRouter>
   )
