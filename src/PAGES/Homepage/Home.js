fb852a20f03a3742bbb0691e4801e958c520034f
chandan pal <chandanpal18222gmail.com>
2024-05-31 14:29:56 +0530

    product pages

--- src/PAGES/Homepage/Home.js
+++ src/PAGES/Homepage/Home.js
@@ -3,8 +3,11 @@ import Navbar from '../../Components/Navbar/Navbar';
 import BannerSlider from '../../Components/Banners/BannerSlider';
 import HomeCategories from '../../Components/Categories/HomeCategories';
 import Footer1 from '../../Components/Footer/Footer';
-
-
+import Product_Sidebar from '../../Components/Product/Product_sidebar';
+import img1 from '../../Assets/Products/pic1.jpg'
+import img2 from '../../Assets/Products/pic2.jpg'
+import img3 from '../../Assets/Products/pic3.jpg'
+import img4 from '../../Assets/Products/pic4.jpg'
 
 
 const Home = () => {
@@ -13,6 +16,7 @@ const Home = () => {
         <Navbar />
         <BannerSlider />
         <HomeCategories />
+        <Product_Sidebar />
         <Footer1 />
         Home
     </div>
